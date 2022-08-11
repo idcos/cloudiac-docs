929a7c1045f0ccb42de887e02106fee1f4a364f5
yangdongyu <yangdongyu@kunxuan.cn>
2022-08-11 10:06:39 +0800

    feat(iac-store): 修改文档中的Registry为Store
    修改文档中的Registry为Store

--- script/cloudiac-k8s.sh
+++ script/cloudiac-k8s.sh
@@ -204,7 +204,7 @@ kubectl apply  -f ./k8s-iac-standard/web.yaml
 
 
 ## 启动runner
-docker run -p 19030:19030 --restart=always --name=runner-01 -v /usr/yunji/cloudiac/var:/usr/yunji/cloudiac/var -v /var/run/docker.sock:/var/run/docker.sock -v /usr/yunji/cloudiac/.env:/usr/yunji/cloudiac/.env -d store.idcos.com/cloudiac/ct-runner:$version
+docker run -p 19030:19030 --restart=always --name=runner-01 -v /usr/yunji/cloudiac/var:/usr/yunji/cloudiac/var -v /var/run/docker.sock:/var/run/docker.sock -v /usr/yunji/cloudiac/.env:/usr/yunji/cloudiac/.env -d registry.idcos.com/cloudiac/ct-runner:$version
 
 echo ""
 echo "The access address is your ip port: [$public_ip:32102]"
